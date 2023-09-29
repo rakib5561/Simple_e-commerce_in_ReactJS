66def5e870a5b808e52ce0a931a863b2ebd81e64
rakib5561 <mdrakib5561@gmail.com>
2023-09-30 03:39:12 +0600

    header part added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import './Header.css';
+import logo from '../../assets/images/Logo.svg';
 
-const Header = () => {
+function Header() {
     return (
-        <div>
-            <h2>Header</h2>
-        </div>
+        <nav className='header'>
+                <img src={logo} alt="" />
+            <div className='menu-items'>
+                <a href="/order">Order</a>
+                <a href="/review">Order Review</a>
+                <a href="/inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
     );
-};
+}
 
 export default Header;
